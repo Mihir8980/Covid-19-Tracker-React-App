ca58393736f8ce17e943769d608d3852e7636be8
Mihir8980 <44092205+Mihir8980@users.noreply.github.com>
2020-07-22 19:19:56 +0530

    Add files via upload

---
+++ build/precache-manifest.62a244930f72ccf9e3468c7c329e05d1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfc37c9e0c4bf70ec9891daae9675a66",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a722f269e7ef4d721a85",
+    "url": "/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "8525f8500ce8251cb2ed",
+    "url": "/static/css/main.3835e3dd.chunk.css"
+  },
+  {
+    "revision": "a722f269e7ef4d721a85",
+    "url": "/static/js/2.4f7a17c5.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/static/js/2.4f7a17c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8525f8500ce8251cb2ed",
+    "url": "/static/js/main.46c4b12b.chunk.js"
+  },
+  {
+    "revision": "3ff5d6fb19a2b7a4acf2",
+    "url": "/static/js/runtime-main.8b39ddf8.js"
+  }
+]);
---
+++ src/InfoBox.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './InfoBox.css';
+import  {
+    CardContent,
+    Card,
+    Typography
+  } from "@material-ui/core";
+
+
+function InfoBox({title,cases,total,active,isRed, ...props}) {
+    return (
+        <Card onClick={props.onClick} className={`infoBox ${active && "infoBox--selected"} ${isRed && "infoBox--red"}`}>
+            <CardContent>
+                <Typography className="infoBox__title" color="textSeconadry">{title}</Typography>
+                <h2 className={`infoBox__cases ${!isRed && "infoBox__cases--green"} `}>{cases}</h2>
+                <Typography className="infoBox__total" color="textSeconadry">{total} Total</Typography>
+            </CardContent>
+        </Card>
+    )
+}
+
+export default InfoBox
